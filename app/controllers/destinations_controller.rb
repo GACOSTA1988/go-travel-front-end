73c2c6a260cea08f65f519973658584c07adc31d
Student <example@example.com>
2020-01-30 13:05:53 -0800

    Add updates

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,5 +4,10 @@ class DestinationsController < ApplicationController
     @destinations = HTTParty.get('http://localhost:3000/destinations', {headers: {Authorization: $hang}})
   end
 
+  def show
+    @destination = HTTParty.get("http://localhost:3000/destinations/#{params[:id]}", {headers: {Authorization: $hang}})
+    @reviews = HTTParty.get("http://localhost:3000/destinations/#{params[:id]}/reviews", {headers: {Authorization: $hang}})
+  end
+
 
 end
